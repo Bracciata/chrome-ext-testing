c7d3f2a3837d8366d92425f36a7929516c279f27
Thomas Braccia <bracciata@gmail.com>
2021-12-10 13:34:12 -0600

    Added a package to test A11y

--- extension/on_search.js
+++ extension/on_search.js
@@ -1,12 +1,26 @@
+const pa11y = require('pa11y');
+
 function getResults(){
     // Note: console.log calls in exectuedScripts show up in the pages stack trace.
     var searchBody = document.getElementById("search");
-    var links = searchBody.getElementsByTagName("a");
+    var linkElements = searchBody.getElementsByTagName("a");
     // TODO: Filter out where role = button
     // TODO: Filter out where href begins with /search
-    for( var link of links){
-        console.log(link);
+    let relevantLinkElements = [];
+    for( var linkElement of linkElements){
+        if(!linkElement.hasAttribute('role') && !linkElement.getAttribute('href').startsWith('/search')){
+            relevantLinkElements += [linkElement];
+        }
     }
+    for(var linkElement of relevantLinkElements){
+        scoreSearchResult(linkElement);
+    }
+}
+function scoreSearchResult(linkElement){
+    pa11y(linkElement.href).then((results) => {
+        console.log(linkElement);
+        console.log(results);
+});
 }
 chrome.webRequest.onCompleted.addListener(function(details) {
     chrome.scripting.executeScript({
